70cf441fbe32feee1eec01cc5463661864d0c31a
dorian <dorianlegan@protonmail.com>
2022-06-01 15:05:59 +0100

    header search

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import "./header.css";
 
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const handleChange = (e) => {
+    const val = e.target.value;
+    setSearchQuery(val);
+  };
   return (
     <div className="header">
       <div className="logo">
         <div className="logoInner">LOGO</div>
       </div>
-      <input placeholder="Find a user" className="searchBox" type={"text"} />
+      <div className="searchWrap">
+        <input
+          placeholder="Find a user"
+          className="searchBox"
+          type={"text"}
+          value={searchQuery}
+          onChange={handleChange}
+        />
+        {searchQuery.length > 0 && <div className={"searchRes"}></div>}
+      </div>
     </div>
   );
 };
